b819892bf63c40ec6472bc29257c3c5fc09182d0
Anton Bagchi <anton.bagchi@vocado.com>
2014-11-06 13:26:58 +0100

    Initial Commit

---
+++ healthkit-poc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  healthkit-poc
+//
+//  Created by Anton Bagchi on 06/11/2014.
+//  Copyright (c) 2014 AKOI Solutions AB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
